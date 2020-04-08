4ba7aa75130d2463239510f13c00e7a2621a4255
arun <arunv04@gmail.com>
2020-04-09 01:38:28 +0530

    Added functionality Updated employee

--- src/main/java/com/customerportal/demo/rest/EmployeeRestController.java
+++ src/main/java/com/customerportal/demo/rest/EmployeeRestController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.customerportal.demo.entity.Employee;
@@ -75,6 +76,21 @@ public class EmployeeRestController {
 		
 		// use a redirect to prevent duplicate submissions
 		return "redirect:/employees/list";
+	}
+	
+	
+	@PostMapping("/showFormForUpdate")
+	public String showFormForUpdate(@RequestParam("employeeId") int theId,
+									Model theModel) {
+		
+		// get the employee from the service
+		Employee theEmployee = employeeService.findById(theId);
+		
+		// set employee as a model attribute to pre-populate the form
+		theModel.addAttribute("employee", theEmployee);
+		
+		// send over to our form
+		return "employees/employee-form";			
 	}
 	
 	
